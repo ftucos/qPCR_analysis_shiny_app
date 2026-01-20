cbb11d4f7b69f1940614a38badb40751fc15506c
ftucos <fran.tucci@gmail.com>
2026-01-20 23:08:18 +0100

    fix: remove duplicate legends in ggplotly

--- R/fix_plotly_legend.R
+++ R/fix_plotly_legend.R
@@ -1,11 +1,17 @@
-# adapted from https://stackoverflow.com/a/59723151/7793290
-fix_plotly_legend <- function(pplot) {
-    for (i in seq_along(pplot$x$data)) {
-        nm <- pplot$x$data[[i]]$name
-        if (!is.null(nm)) {
-            pplot$x$data[[i]]$name <- sub("^\\(([^,]+),.*\\)$", "\\1", nm)
-        }
+# adapted from https://stackoverflow.com/a/69035732/7793290
+fix_plotly_legend <- function(gp) {
+    for (i in seq_along(gp$x$data)) {
+        # Is the layer the first entry of the group?
+        is_first <- grepl("^\\(.*?,1\\)", gp$x$data[[i]]$name)
+        
+        nm <- gp$x$data[[i]]$name
+        # Extract the group identifier and assign it to the name and legendgroup arguments
+        gp$x$data[[i]]$name <- gsub("^\\((.*?),\\d+\\)", "\\1", nm)
+        gp$x$data[[i]]$legendgroup <- gp$x$data[[i]]$name
+        # Show the legend only for the first layer of the group 
+        if (!is_first) gp$x$data[[i]]$showlegend <- FALSE
     }
     
-    pplot
+    gp
 }
+
